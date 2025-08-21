cd1af73c81b7496be8474f9c17e69a7a18a98d85
pshreyaan <shreyaan.pradhan@elemica.com>
2025-08-21 06:38:12 +0530

    changes made

--- src/test/java/validate/ValidateSamples.java
+++ src/test/java/validate/ValidateSamples.java
@@ -2,25 +2,46 @@ package validate;
 
 import base.DriverManagerBase;
 import components.SupplierPortal;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import pages.login.NetworkLoginFlow;
 import pages.login.SupplierLoginFlow;
+import utils.ConfigReader;
 
 public class ValidateSamples extends DriverManagerBase {
 
-    private SupplierPortal portal;
     private static final String XML_DIR = "resources/xml";
-
+	private SupplierLoginFlow portal;
+	private NetworkLoginFlow network;
+    private String url;
+    private String userName;
+    private String password;
+    
     @BeforeClass
     public void loginAndNavigateOnce() {
-        // Login exactly like UploadSample
-        SupplierLoginFlow login = new SupplierLoginFlow(getDriver());
-        login.login();
-
-        // Navigate to Integration Log (single action lives in components.SupplierPortal)
-        portal = new SupplierPortal(getDriver());
-        portal.navigateIntegrationLog();
+	    // Suppress Selenium CDP logging
+	    java.util.logging.Logger.getLogger("org.openqa.selenium.devtools.CdpVersionFinder").setLevel(java.util.logging.Level.OFF);
+	    java.util.logging.Logger.getLogger("org.openqa.selenium.chromium.ChromiumDriver").setLevel(java.util.logging.Level.OFF);
+	    
+	    this.portal = new SupplierLoginFlow();
+	    this.network = new NetworkLoginFlow();
+	    
+	    url = ConfigReader.get("supplier_portal");
+        userName = ConfigReader.get("supplier_portal_UserName");
+        password = ConfigReader.get("supplier_portal_Password");
+        
+        System.out.println("Java Runtime version: " + System.getProperty("java.version"));
+        Package seleniumPackage = RemoteWebDriver.class.getPackage();
+        System.out.println("Selenium version: " + seleniumPackage.getImplementationVersion());
+        Package wdmPackage = WebDriverManager.class.getPackage();
+        System.out.println("WebDriverManager version: " + wdmPackage.getImplementationVersion());
+        
+        portal.loginFlow(driver, userName, password, url);
     }
 
     // Keep only the files you want to validate "for now"
@@ -31,19 +52,17 @@ public class ValidateSamples extends DriverManagerBase {
             {"orderCancel01_updated.xml"},
             {"orderChange01_updated.xml"},
             {"orderMix01_updated.xml"},
-            {"case07_updated.xml"},
-            {"case08_updated.xml"},
-            {"case09_updated.xml"},
         };
     }
 
-    @Test(dataProvider = "samples")
-    public void validateOneSample(String xmlFile) {
-        IntegrationLogVerification.validateAndPersist(
-                getDriver(),
-                portal,
-                XML_DIR,
-                xmlFile
-        );
+	/*    
+	 * UC1_UploadOrderCreate validation
+	*/
+    @Test
+    public void UC1_UploadOrderCreateValidation() throws InterruptedException{
+    	System.out.println("UseCase 1 : Upload Order Create Validation");
+    	
+    	
     }
+
 }
